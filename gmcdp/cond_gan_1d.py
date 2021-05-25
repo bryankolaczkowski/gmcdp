ca363376c7dcdc144575b5cf6aed8ce89f8e0e10
Bryan Kolaczkowski <bryank@ufl.edu>
2021-05-24 20:38:33 -0400

    trying nadam

--- gmcdp/cond_gan_1d.py
+++ gmcdp/cond_gan_1d.py
@@ -213,8 +213,8 @@ if __name__ == '__main__':
   data = data.shuffle(ndata).batch(batchsize)
 
   # create optimizer
-  gopt = tf.keras.optimizers.Adam(learning_rate=5.0e-6, beta_1=0.5, beta_2=0.9)
-  dopt = tf.keras.optimizers.Adam(learning_rate=1.0e-5, beta_1=0.5, beta_2=0.9)
+  gopt = tf.keras.optimizers.Nadam(learning_rate=1.0e-6, beta_1=0.9, beta_2=0.99)
+  dopt = tf.keras.optimizers.Nadam(learning_rate=1.0e-5, beta_1=0.9, beta_2=0.99)
   opt  = GanOptimizer(gopt, dopt)
 
   # create gan
