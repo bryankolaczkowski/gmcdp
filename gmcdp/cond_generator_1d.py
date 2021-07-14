6a49c785e5b4fe2a6016de210d5e58656e832dc3
Bryan Kolaczkowski <bryank@ufl.edu>
2021-07-14 18:08:07 -0400

    smaller gen?

--- gmcdp/cond_generator_1d.py
+++ gmcdp/cond_generator_1d.py
@@ -150,22 +150,15 @@ class EncodeGen(EncodeLayer):
     return tf.concat((ps, lp), axis=-1)
 
 
-class DecodeGen(ReluLayer):
+class DecodeGen(WidthLayer):
   """
   decodes generator output to data
   """
   def __init__(self, *args, **kwargs):
     super(DecodeGen, self).__init__(*args, **kwargs)
     # construct
-    self.dpr = tf.keras.layers.Dense(units=1,
-                                    use_bias=self.use_bias,
-                                    kernel_initializer=self.kernel_initializer,
-                                    bias_initializer=self.bias_initializer,
-                                    kernel_regularizer=self.kernel_regularizer,
-                                    bias_regularizer=self.bias_regularizer,
-                                    kernel_constraint=self.kernel_constraint,
-                                    bias_constraint=self.bias_constraint)
-    self.out = tf.keras.layers.Dense(units=self.width,
+    self.out = tf.keras.layers.LocallyConnected1D(filters=1,
+                                    kernel_size=1,
                                     use_bias=self.use_bias,
                                     kernel_initializer=self.kernel_initializer,
                                     bias_initializer=self.bias_initializer,
@@ -177,8 +170,7 @@ class DecodeGen(ReluLayer):
     return
 
   def call(self, inputs):
-    x = self.flt(gnact(self.dpr(inputs), alpha=self.relu_alpha))
-    return self.out(x)
+    return self.flt(self.out(inputs))
 
 
 class PosMaskedMHABlock(ReluLayer):
